4a90e4ab697c2a591eeefa55d4b530df935d296d
Oded Breiner <https://linkedin.com/in/odedhb>
2014-10-06 19:00:52 -0700

    Simplified voice API

--- SmartNoteApp/app/src/main/java/listen/api/Listen.java
+++ SmartNoteApp/app/src/main/java/listen/api/Listen.java
@@ -1,13 +1,11 @@
 package listen.api;
 
-import android.content.Intent;
-
 /**
  * Created by oded on 10/6/14.
  */
 public class Listen {
 
-    Listen(Intent... intent) {
-
+    Listen addListenIntents(ListenIntent... listenIntent) {
+        return this;
     }
 }
