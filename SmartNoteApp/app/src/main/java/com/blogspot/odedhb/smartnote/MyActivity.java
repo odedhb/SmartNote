3f890e59dbbefbecf0885c33d4b31ff7217affa4
Oded Breiner <https://linkedin.com/in/odedhb>
2014-10-10 09:32:05 -0700

    Create button
    For use without a watch.

--- SmartNoteApp/app/src/main/java/com/blogspot/odedhb/smartnote/MyActivity.java
+++ SmartNoteApp/app/src/main/java/com/blogspot/odedhb/smartnote/MyActivity.java
@@ -1,7 +1,12 @@
 package com.blogspot.odedhb.smartnote;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.blogspot.odedhb.smartnote.model.Item;
@@ -34,4 +39,36 @@ public class MyActivity extends Activity {
     }
 
 
+    public void createItem(View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Title");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        builder.setView(input);
+
+// Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String text = input.getText().toString();
+                if (text == null || text.length() < 1) {
+                    return;
+                }
+                new Item(text, System.currentTimeMillis()).save();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+
+    }
 }
