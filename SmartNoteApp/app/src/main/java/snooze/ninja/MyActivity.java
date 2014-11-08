5740c6d2d85a65aed408f6a593dbe6a516c8f9ef
Oded Breiner <https://linkedin.com/in/odedhb>
2014-11-08 11:41:42 -0800

    Feature: Create button works!

--- SmartNoteApp/app/src/main/java/snooze/ninja/MyActivity.java
+++ SmartNoteApp/app/src/main/java/snooze/ninja/MyActivity.java
@@ -1,19 +1,21 @@
 package snooze.ninja;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import snooze.ninja.SpeechDating.SpeechDate;
 import snooze.ninja.SpeechDating.TimeHypotheses;
 import snooze.ninja.controller.ItemAdapter;
-import snooze.ninja.model.CreateNotification;
 import snooze.ninja.model.Item;
 
 
@@ -53,6 +55,48 @@ public class MyActivity extends ActionBarActivity {
         mItemAdapter.notifyDataSetChanged();
     }
 
+    public void addButton(View view) {
+
+        EditText createBox = (EditText) findViewById(R.id.create_edit_text);
+
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInputFromWindow(createBox.getApplicationWindowToken(), InputMethodManager.SHOW_FORCED, 0);
+
+        addItem(createBox);
+
+    }
+
+    void addItem(TextView textView) {
+
+        String text = textView.getText().toString();
+        if (text == null || text.length() < 1) {
+            return;
+        }
+
+        SpeechDate speechDate = new SpeechDate(text);
+        TimeHypotheses selectedHypotheses = speechDate.getSelectedHypotheses();
+        Long time;
+        Item item;
+        if (selectedHypotheses == null) {
+            item = new Item(text);
+        } else {
+            time = selectedHypotheses.getTimeInMillis();
+            item = new Item(text, time);
+        }
+
+        item.save();
+        int position = item.getPositionInList();
+
+        if (position == 0) {
+            mItemAdapter.notifyDataSetChanged();
+        } else {
+            mItemAdapter.notifyItemInserted(position);
+        }
+
+        textView.setText("");
+
+    }
+
 
     class EditorActionDoneListener implements TextView.OnEditorActionListener {
 
@@ -61,35 +105,7 @@ public class MyActivity extends ActionBarActivity {
 
             if (actionId != EditorInfo.IME_ACTION_DONE) return false;
 
-            String text = textView.getText().toString();
-            if (text == null || text.length() < 1) {
-                return false;
-            }
-
-            SpeechDate speechDate = new SpeechDate(text);
-            TimeHypotheses selectedHypotheses = speechDate.getSelectedHypotheses();
-            Long time;
-            Item item;
-            if (selectedHypotheses == null) {
-                item = new Item(text);
-            } else {
-                time = selectedHypotheses.getTimeInMillis();
-                item = new Item(text, time);
-            }
-
-            item.save();
-            int position = item.getPositionInList();
-
-            if (position == 0) {
-                mItemAdapter.notifyDataSetChanged();
-            } else {
-                mItemAdapter.notifyItemInserted(position);
-            }
-
-            /*InputMethodManager imm = (InputMethodManager) getSystemService(
-                    INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(textView.getWindowToken(), 0);*/
-            textView.setText("");
+            addItem(textView);
 
             return true;
         }
